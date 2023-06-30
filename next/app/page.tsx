a9288403004133d1292acc04c43246ef1adb240d
kay <hallokay044@gmail.com>
2023-06-30 16:01:03 +0900

    Revert "2강"
    This reverts commit 4bdc419f2a6922af75e7bfe2f211915ace247e0f.

--- next/app/page.tsx
+++ next/app/page.tsx
@@ -3,7 +3,7 @@ import Link from 'next/link';
 
 export default function Home() {
   return (
-    <main className={styles.main}>
+    <main>
       <h1>hi</h1>
       <Link href="/about">about</Link>
 
