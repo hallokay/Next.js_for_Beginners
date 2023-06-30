8ffdd1484f29010308914401991132b44f14febd
kay <hallokay044@gmail.com>
2023-06-30 16:55:36 +0900

    3강 data fetching 진행중

--- next03/app/page.tsx
+++ next03/app/page.tsx
@@ -1,16 +1,12 @@
 import styles from './page.module.css'
-import Link from 'next/link';
-
+import Link from 'next/link'
 export default function Home() {
   return (
     <main className={styles.main}>
-      <h1>hi</h1>
+      <h1>hihi</h1>
       <p>
-
         <Link href="/users">Users</Link>
-
       </p>
-
     </main>
   )
 }
