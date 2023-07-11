59a23df9d9535b2041e527eb7ac9e1f55acc2ef7
hallokay <hallokay044@gmail.com>
2023-07-11 14:36:28 +0900

    작업중

--- next06-blog/app/layout.tsx
+++ next06-blog/app/layout.tsx
@@ -1,11 +1,9 @@
 import "./globals.css";
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import { Navbar, MyProfilePic } from "./componets";
 
 export const metadata: Metadata = {
-  title: "Next.js blog",
+  title: "Greenbean's blog",
   description: "blog website",
 };
 
@@ -16,7 +14,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko" className="dark">
-      <body className="dark:bg-slate-800">{children}</body>
+      <body className="dark:bg-slate-800">
+        <Navbar />
+        <MyProfilePic />
+        {children}</body>
     </html>
   );
 }
